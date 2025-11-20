d8f1b1bbe7e521567609ef5e6ef47a7ef5d8bb20
ziming <ziming@users.noreply.github.com>
2025-11-20 22:37:48 +0800

    remove package service provider class as we don't really need it

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,14 +5,7 @@ declare(strict_types=1);
 namespace Ziming\LaravelMemoryHealthCheck\Tests;
 
 use Orchestra\Testbench\TestCase as Orchestra;
-use Ziming\LaravelMemoryHealthCheck\LaravelMemoryHealthCheckServiceProvider;
 
 class TestCase extends Orchestra
 {
-    protected function getPackageProviders($app): array
-    {
-        return [
-            LaravelMemoryHealthCheckServiceProvider::class,
-        ];
-    }
 }
